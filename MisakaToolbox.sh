fb7e18ebe72045066188d7a406fd79877aae32c5
Misaka No <96560028+misakano7545@users.noreply.github.com>
2022-01-01 09:13:35 +0800

    Update MisakaToolbox.sh

--- MisakaToolbox.sh
+++ MisakaToolbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 一些全局变量
-ver="1.4.4"
-changeLog="在主菜单提示VPS信息，并新增部署Telegram MTProxy脚本"
+ver="1.4.5"
+changeLog="新增禁用Oracle系统自带防火墙、Acme.sh和Screen后台任务管理脚本"
 arch=`uname -m`
 virt=`systemd-detect-virt`
 kernelVer=`uname -r`
@@ -20,56 +20,56 @@ yellow(){
 }
 
 if [[ -f /etc/redhat-release ]]; then
-release="Centos"
+    release="Centos"
 elif cat /etc/issue | grep -q -E -i "debian"; then
-release="Debian"
+    release="Debian"
 elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-release="Ubuntu"
+    release="Ubuntu"
 elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-release="Centos"
+    release="Centos"
 elif cat /proc/version | grep -q -E -i "debian"; then
-release="Debian"
+    release="Debian"
 elif cat /proc/version | grep -q -E -i "ubuntu"; then
-release="Ubuntu"
+    release="Ubuntu"
 elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-release="Centos"
+    release="Centos"
 else 
-red "不支持你当前系统，请使用Ubuntu、Debian、Centos的主流系统"
-rm -f MisakaToolbox.sh
-exit 1
+    red "不支持你当前系统，请使用Ubuntu、Debian、Centos的主流系统"
+    rm -f MisakaToolbox.sh
+    exit 1
 fi
 
 if ! type curl >/dev/null 2>&1; then 
-yellow "curl未安装，安装中"
-if [ $release = "Centos" ]; then
-yum -y update && yum install curl -y
+    yellow "curl未安装，安装中"
+    if [ $release = "Centos" ]; then
+        yum -y update && yum install curl -y
+    else
+        apt-get update -y && apt-get install curl -y
+    fi	   
 else
-apt-get update -y && apt-get install curl -y
-fi	   
-else
-green "curl已安装"
+    green "curl已安装"
 fi
 
 if ! type wget >/dev/null 2>&1; then 
-yellow "wget未安装，安装中"
-if [ $release = "Centos" ]; then
-yum -y update && yum install wget -y
+    yellow "wget未安装，安装中"
+    if [ $release = "Centos" ]; then
+        yum -y update && yum install wget -y
+    else
+        apt-get update -y && apt-get install wget -y
+    fi	   
 else
-apt-get update -y && apt-get install wget -y
-fi	   
-else
-green "wget已安装"
+    green "wget已安装"
 fi
 
 if ! type sudo >/dev/null 2>&1; then 
-yellow "sudo未安装，安装中"
-if [ $release = "Centos" ]; then
-yum -y update && yum install sudo -y
+    yellow "sudo未安装，安装中"
+    if [ $release = "Centos" ]; then
+        yum -y update && yum install sudo -y
+    else
+        apt-get update -y && apt-get install sudo -y
+    fi	   
 else
-apt-get update -y && apt-get install sudo -y
-fi	   
-else
-green "sudo已安装"
+    green "sudo已安装"
 fi
 
 function rootLogin(){
@@ -170,14 +170,39 @@ function hijk(){
     bash <(curl -sL https://raw.githubusercontent.com/Misaka-blog/hijk-backup/master/xray.sh)
 }
 
-function updateScript(){
-    wget -N https://raw.githubusercontent.com/Misaka-blog/MisakaLinuxToolbox/master/MisakaToolbox.sh && chmod -R 777 MisakaToolbox.sh && bash MisakaToolbox.sh
-}
-
 function tgMTProxy(){
     mkdir /home/mtproxy && cd /home/mtproxy
     curl -s -o mtproxy.sh https://raw.githubusercontent.com/sunpma/mtp/master/mtproxy.sh && chmod +x mtproxy.sh && bash mtproxy.sh
     bash mtproxy.sh start
+}
+
+function acme1key(){
+    wget -N https://cdn.jsdelivr.net/gh/Misaka-blog/acme1key@master/acme1key.sh && chmod -R 777 acme1key.sh && bash acme1key.sh
+}
+
+function screenManager(){
+    wget -N https://cdn.jsdelivr.net/gh/Misaka-blog/screenManager@master/screen.sh && chmod -R 777 screen.sh && bash screen.sh
+}
+
+function oraclefirewall(){
+    if [ $release = "Centos" ]; then
+        systemctl stop oracle-cloud-agent
+        systemctl disable oracle-cloud-agent
+        systemctl stop oracle-cloud-agent-updater
+        systemctl disable oracle-cloud-agent-updater
+        systemctl stop firewalld.service
+        systemctl disable firewalld.service
+    else
+        iptables -P INPUT ACCEPT
+        iptables -P FORWARD ACCEPT
+        iptables -P OUTPUT ACCEPT
+        iptables -F
+        apt-get purge netfilter-persistent -y
+    fi
+}
+
+function updateScript(){
+    wget -N https://raw.githubusercontent.com/Misaka-blog/MisakaLinuxToolbox/master/MisakaToolbox.sh && chmod -R 777 MisakaToolbox.sh && bash MisakaToolbox.sh
 }
 
 function start_menu(){
@@ -199,20 +224,28 @@ function start_menu(){
     yellow "操作系统：$release"
     yellow "内核版本：$kernelVer"
     echo "                            "
-    echo "下面是我们提供的一些功能"
+    green "下面是工具箱提供的一些功能"
+    echo "                            "
     echo "1. VPS修改登录方式为root密码登录"
     echo "2. VPS安装warp"
     echo "3. X-ui面板安装"
     echo "4. Mack-a 节点配置脚本"
+    echo "                            "
     echo "5. 一键开启BBR"
     echo "6. 安装宝塔开心版"
     echo "7. 一键安装docker"
     echo "8. 流媒体解锁测试"
+    echo "                            "
     echo "9. VPS三网测速"
     echo "10. 修改主机名"
     echo "11. 安装可乐大佬的ServerStatus-Horatu探针"
     echo "12. hijk大佬的v2脚本，支持IBM LinuxONE s390x的机器搭建节点"
+    echo "                            "
     echo "13. 一键安装 Telegram MTProxy 代理服务器"
+    echo "14. Acme.sh 证书申请脚本"
+    echo "15. Screen 后台运行管理脚本"
+    echo "16. 禁用Oracle（甲骨文）系统自带防火墙"
+    echo "                            "
     echo "v. 更新脚本"
     echo "0. 退出脚本"
     echo "                            "
